45c4aeb7786706571c8b45001063ee755cfca894
Abdul-Moeed <12bscsamoeed@seecs.edu.pk>
2015-02-19 22:47:47 +0500

    Update Matrix.java

--- src/ap_lab1/Matrix.java
+++ src/ap_lab1/Matrix.java
@@ -4,8 +4,10 @@ package ap_lab1;
  *
  * @author Abdul Moeed
  */
+
+//matrix class holds nxn matrix
 public class Matrix {
-    int num_rows;
-    int num_cols;
-    int [][] vals;
+    int num_rows;   // rows in matrix
+    int num_cols;   // columns in matrix
+    int [][] vals;  // values inside matrix
 }
